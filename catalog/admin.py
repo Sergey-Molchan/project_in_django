06a375c7aa80b5b47a761fb8fe50ae77ff5bba4b
Sergey <molchan2025@gmail.com>
2025-10-16 13:49:28 -0700

    добавлены миграции и модели каталога и продуктов

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # ← id и name как в ТЗ!
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')  # ← id, name, price, category
+    list_filter = ('category',)  # ← фильтрация по категории
+    search_fields = ('name', 'description')  # ← поиск по name и description
--- config/settings.py
+++ config/settings.py
@@ -1,15 +1,17 @@
-
+from dotenv import load_dotenv
 from pathlib import Path
+import os
 
-from django.conf.global_settings import STATICFILES_DIRS
+
+load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-o=k8@41p)6scj6bz@lh=r$*ekh3yd!y6p$o#l2enk4y#&gl==3'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = []
 
@@ -62,10 +64,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
+
 
 
 # Password validation
@@ -107,3 +114,6 @@ STATICFILES_DIRS = [
     ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
