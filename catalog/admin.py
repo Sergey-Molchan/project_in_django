6d61220070fd711fe9116e7809cdfdc354edc50f
Sergey <molchan2025@gmail.com>
2025-10-19 13:46:53 -0700

    фикстуры json файлами добавлены

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Contact
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -10,3 +10,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')  # ← id, name, price, category
     list_filter = ('category',)  # ← фильтрация по категории
     search_fields = ('name', 'description')  # ← поиск по name и description
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['address', 'phone', 'email', 'updated_at']
+
+    def has_add_permission(self, request):
+        # Разрешаем создание только если нет существующих записей
+        return Contact.objects.count() == 0
