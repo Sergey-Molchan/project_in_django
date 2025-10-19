ca9fb96118153f3f25eb1fdef50c80d7244237d9
Sergey <molchan2025@gmail.com>
2025-10-19 13:46:53 -0700

    фикстуры json файлами добавлены

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 
 def home(request):
-    return render(request, 'home.html')
+    categories_count = Category.objects.count()
+    products_count = Product.objects.count()
+
+    context = {
+        'categories_count': categories_count,
+        'products_count': products_count,
+    }
+
+    return render(request, 'home.html', context)
 
 
 def contacts(request):
