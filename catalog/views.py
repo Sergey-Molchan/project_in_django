3206c24e8bb2bf7dad6efc432f50d87e76f89003
Sergey <molchan2025@gmail.com>
2025-11-07 17:42:30 -0800

    Добавлена реализация форм

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
-from django.views.generic import UpdateView, DetailView, CreateView, TemplateView, ListView
+from django.views.generic import UpdateView, DetailView, CreateView, TemplateView, ListView, DeleteView
+from .forms import ProductForm
+from django.urls import reverse_lazy
+
 
 
 class HomeView(TemplateView):
@@ -13,7 +17,7 @@ class HomeView(TemplateView):
         return context
 
 
-class ContactsViev(TemplateView):
+class ContactsView(TemplateView):
     template_name = 'contacts.html'
 
 
@@ -44,3 +48,31 @@ class HouseGardenView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'product_detail.html'
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:index')  # Или на страницу продуктов
+
+    def form_valid(self, form):
+        # Автоматически устанавливаем создателя продукта
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:index')
+
