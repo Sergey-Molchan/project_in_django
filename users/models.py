429fec895e3a752cf70eb2433c837fd0fb5aee50
Sergey <molchan2025@gmail.com>
2025-11-16 12:57:40 +0300

    Реализована функция авторизации по почте, добавлена кастомка удаления авторизованных

--- users/models.py
+++ users/models.py
@@ -1,17 +1,24 @@
+# users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
-
+import uuid
 
 
 class User(AbstractUser):
+    DoesNotExist = None
     username = None
-    email = models.EmailField(unique= True, verbose_name='Email')
+    email = models.EmailField(unique=True, verbose_name='Email')
     phone_number = PhoneNumberField(blank=True, verbose_name='Телефон', help_text='Введите номер телефона')
-    tg_name = models.CharField(max_length=50, verbose_name='Telegram', blank=True, null=True, help_text='Введите ваш телеграм ник')
-    avatar = models.ImageField(upload_to='users/avatars', blank=True, null=True, verbose_name='Аватарка', help_text='Загрузите фото профиля')
+    tg_name = models.CharField(max_length=50, verbose_name='Telegram', blank=True, null=True,
+                               help_text='Введите ваш телеграм ник')
+    avatar = models.ImageField(upload_to='users/avatars', blank=True, null=True, verbose_name='Аватарка',
+                               help_text='Загрузите фото профиля')
     token = models.CharField(max_length=100, verbose_name='Token', blank=True, null=True)
 
+    # Поля для верификации
+    email_verified = models.BooleanField(default=False, verbose_name='Email подтвержден')
+    is_active = models.BooleanField(default=False, verbose_name='Активный')  # По умолчанию неактивен
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -19,6 +26,12 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = 'Пользователи'
+
+    def save(self, *args, **kwargs):
+        # Автоматически генерируем token при создании пользователя
+        if not self.token:
+            self.token = str(uuid.uuid4())
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.email
--- users/views.py
+++ users/views.py
@@ -1,34 +1,77 @@
 from django.core.mail import send_mail
-from django.shortcuts import redirect, get_object_or_404
+from django.conf import settings
+from django.contrib import messages
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView
-from users.forms import UsersRegisterForm
-from users.models import User
-import secrets
-from config.settings import EMAIL_HOST_USER
+from django.views.generic import CreateView, TemplateView
+from django.shortcuts import redirect, get_object_or_404
+from .forms import UsersRegisterForm
+from .models import User
 
 
 class UserCreateView(CreateView):
     model = User
     form_class = UsersRegisterForm
-    success_url = reverse_lazy('users:login')
     template_name = 'users/user_form.html'
 
     def form_valid(self, form):
-        user = form.save()
-        user.is_active = False
-        token = secrets.token_hex(16)
-        host = self.request.get_host()
-        url = f'http://{host}/users.email-confirm/{token}/'
-        send_mail(
-            subject='Подтверждение почты',
-            message=f"Здравствуйте! спасибо что зарегистрировались в нашем магазине. Перейдите по ссылке для окончания регистрации {url}",
-            from_email=EMAIL_HOST_USER,
-            recipient_list=[user.email]
-        )
-        return super().form_valid(form)
+        # Сохраняем пользователя как НЕактивного
+        self.object = form.save(commit=False)
+        self.object.is_active = False  # Блокируем доступ до верификации
+        # Token автоматически создается в методе save() модели
+        self.object.save()
 
-def email_verification(requests, token):
-    user = User.get_object_or_404(User, token=token)
-    User.is_active = True
-    return redirect(reverse('users:login'))
+        # Отправляем email с ссылкой для верификации
+        try:
+            verification_url = self.request.build_absolute_uri(
+                reverse('users:email-confirm', kwargs={'token': self.object.token})
+            )
+
+            send_mail(
+                subject='Подтверждение email в нашем магазине',
+                message=f'''Здравствуйте, {self.object.email}!
+
+Для завершения регистрации подтвердите ваш email, перейдя по ссылке:
+{verification_url}
+
+Ссылка действительна всегда.
+
+Если вы не регистрировались в нашем магазине, проигнорируйте это письмо.
+
+С уважением,
+Команда магазина''',
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[self.object.email],
+                fail_silently=False,
+            )
+            # Перенаправляем на страницу "письмо отправлено"
+            return redirect('users:email_verification_sent')
+
+        except Exception as e:
+            # Если email не отправился, удаляем пользователя
+            self.object.delete()
+            messages.error(self.request, f'Ошибка при отправке email: {str(e)}')
+            return self.form_invalid(form)
+
+    def get_success_url(self):
+        # Этот метод больше не используется, так как мы делаем redirect в form_valid
+        return reverse_lazy('users:email_verification_sent')
+
+
+class EmailVerificationSentView(TemplateView):
+    template_name = 'users/email_verification_sent.html'
+
+
+def email_verification(request, token):
+    try:
+        user = get_object_or_404(User, token=token)
+        if not user.email_verified:
+            user.email_verified = True
+            user.is_active = True  # Активируем после верификации
+            user.save()
+            messages.success(request, '✅ Ваш email успешно подтвержден! Теперь вы можете войти в систему.')
+        else:
+            messages.info(request, '✅ Ваш email уже подтвержден.')
+    except User.DoesNotExist:
+        messages.error(request, '❌ Неверная или устаревшая ссылка подтверждения.')
+
+    return redirect('users:login')
