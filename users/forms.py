618199f3ffe0703d848f8bb15daf1d0514061633
Sergey <molchan2025@gmail.com>
2025-11-16 10:25:04 +0300

    Добавлена основная реализация аутентификации

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,54 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from users.models import User
-from catalog.models import StyleFormMixin
+from django.core.exceptions import ValidationError
 
 
+class UsersRegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите ваш email'
+        }),
+        help_text="Обязательное поле. Введите действующий email адрес."
+    )
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите пароль'
+        }),
+        help_text="""
+        <ul class="form-text">
+            <li>Пароль не должен быть слишком похож на другую вашу личную информацию.</li>
+            <li>Пароль должен содержать как минимум 8 символов.</li>
+            <li>Пароль не должен быть слишком простым и распространенным.</li>
+            <li>Пароль не может состоять только из цифр.</li>
+        </ul>
+        """
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Повторите пароль'
+        }),
+        help_text="Для подтверждения введите, пожалуйста, пароль ещё раз."
+    )
 
-class UsersRegisterForm(StyleFormMixin, UserCreationForm):
-    model = User
-    fields = ['email', "password1", 'password2']
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Удаляем поле username из формы
+        if 'username' in self.fields:
+            del self.fields['username']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Пользователь с таким email уже существует.")
+        return email
--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
+from users.forms import UsersRegisterForm
+from users.models import User
+import secrets
+from config.settings import EMAIL_HOST_USER
 
-# Create your views here.
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UsersRegisterForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/user_form.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        host = self.request.get_host()
+        url = f'http://{host}/users.email-confirm/{token}/'
+        send_mail(
+            subject='Подтверждение почты',
+            message=f"Здравствуйте! спасибо что зарегистрировались в нашем магазине. Перейдите по ссылке для окончания регистрации {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+        return super().form_valid(form)
+
+def email_verification(requests, token):
+    user = User.get_object_or_404(User, token=token)
+    User.is_active = True
+    return redirect(reverse('users:login'))
